92919467f0c9811d9b7d02c30a75617eb1b1f4bd
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-09-27 12:55:33 -0400

    Update the scraper due to website changes since article was first written. Logic to detect jobs loaded now watches the span that displays how many jobs are in the results to determine when the jobs table gets updated.

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import re
 import urlparse
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 from time import sleep
 
@@ -13,25 +14,36 @@ class TaleoJobScraper(object):
     def __init__(self):
         self.driver = webdriver.PhantomJS()
         self.driver.set_window_size(1120, 550)
+        self.wait = WebDriverWait(self.driver, 10)
 
     def scrape_job_links(self):
         self.driver.get(link)
+
+        r = re.compile(r'\d+ - \d+ of \d+')
+        f = lambda d: re.search(r, d.find_element_by_id('currentPageInfo').text)
+
+        self.wait.until(f)
+
+        m = f(self.driver)
+        t = m.group(0)
 
         jobs = []
         pageno = 2
 
         while True:
-            s = BeautifulSoup(self.driver.page_source)
-            r = re.compile(r'jobdetail\.ftl\?job=\d+$')
+            print 'Scraping page %d' % (pageno - 1)
 
-            for a in s.findAll('a', href=r):
+            s = BeautifulSoup(self.driver.page_source)
+            h = re.compile(r'jobdetail\.ftl\?job=\d+')
+
+            for a in s.findAll('a', href=h):
                 tr = a.findParent('tr')
                 td = tr.findAll('td')
 
                 job = {}
                 job['title'] = a.text
                 job['url'] = urlparse.urljoin(link, a['href'])
-                job['location'] = td[2].text
+                job['location'] = td[1].text
                 jobs.append(job)
 
             next_page_elem = self.driver.find_element_by_id('next')
@@ -39,8 +51,14 @@ class TaleoJobScraper(object):
 
             if next_page_link:
                 next_page_elem.click()
+
+                # Watch for the transition from "1 - 25 of 1106" to "26 - 50 of 1106"
+                self.wait.until(lambda d: f(d) and f(d).group(0) != t)
+
+                m = f(self.driver)
+                t = m.group(0)
+
                 pageno += 1
-                sleep(.75)
             else:
                 break
 
